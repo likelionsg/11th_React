4be7760395da0c00483796bcfdf362277beb3343
dianexjeong <zuphyr14@gmail.com>
2023-05-12 23:34:52 +0900

    chore: update app.js template code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Main, MediaDiv } from "./styledComponents";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      {/* Global Styling */}
+      <MediaDiv>
+        {/* Header */}
+        <Main>
+          {/* Slogan */}
+          {/* Routes */}
+        </Main>
+      </MediaDiv>
+      {/* Footer */}
+    </>
+  );
 };
 
 export default App;
